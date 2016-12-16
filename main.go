1f261a13983a6f50412838393a793d204b0cd689
Karan Sangha <iamkaransangha@gmail.com>
2016-12-15 16:29:15 -0800

    Parse command line argument

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
+import "flag"
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	var isHost bool
+
+	flag.BoolVar(&isHost, "listen", false, "Listens on the specified IP address")
+	flag.Parse()
+
+	if isHost {
+		fmt.Println("is host")
+	} else {
+		fmt.Println("is not host")
+	}
 }
