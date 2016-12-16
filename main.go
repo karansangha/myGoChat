06393e2326e18e636322b25a710ec18b782b043f
Karan Sangha <iamkaransangha@gmail.com>
2016-12-15 17:18:50 -0800

    Refactored RunHost and RunGuest to a separate package

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import "flag"
-
 import "os"
-import "net"
 
-import "log"
-import "bufio"
-import "fmt"
+import "./lib"
 
 func main() {
 	var isHost bool
@@ -18,40 +14,10 @@ func main() {
 	if isHost {
 		// go run main.go -listen <ip>
 		connIP := os.Args[2]
-		runHost(connIP)
+		lib.RunHost(connIP)
 	} else {
 		// go run main.go <ip>
 		connIP := os.Args[1]
-		runGuest(connIP)
+		lib.RunGuest(connIP)
 	}
-}
-
-const port = "8080"
-
-func runHost(ip string) {
-	ipAndPort := ip + ":" + port
-	listener, listenErr := net.Listen("tcp", ipAndPort)
-
-	if listenErr != nil {
-		log.Fatal("Error: ", listenErr)
-	}
-
-	conn, acceptErr := listener.Accept()
-
-	if acceptErr != nil {
-		log.Fatal("Error: ", acceptErr)
-	}
-
-	reader := bufio.NewReader(conn)
-	message, readErr := reader.ReadString('\n')
-
-	if readErr != nil {
-		log.Fatal("Error: ", readErr)
-	}
-
-	fmt.Println("Message received: ", message)
-}
-
-func runGuest(ip string) {
-
 }
